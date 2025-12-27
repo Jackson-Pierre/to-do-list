fdb25d0dd3ba5ba24ef266226ce2c45cf037bbdd
Jackson <jacksonppereira4@gmail.com>
2025-12-27 16:35:23 -0400

    Começaando Projeto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Title } from "./styles"
+
+function App() {
+  
+
+
+  return(
+    <>
+      <input placeholder="O que eu tenho que fazer..." type="text" />
+      <button>Adicionar</button>
+
+      <ul>
+        
+      </ul>
+    </>
+  )
+}
+
+export default App
