50f783e264f0dea604bfced37eacae17659dd15b
Jackson <jacksonppereira4@gmail.com>
2025-12-27 17:43:30 -0400

    Adicionando algumas funcionabilidades

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import { Title } from "./styles"
+import React, { useState } from "react"
+import { v4 as uuid } from 'uuid'
 
 function App() {
-  
+  const [List, setList] = useState([{id: uuid(), task: "Nada"}])
+  const [task, setTask] = useState("")
+//"Cuidar da Laislly", "Estudar React", "Estudar JS"
+
+  function palavraAdicionnada(event) {
+    setTask(event.target.value)    
+  }
+
+  function adicioneiPalavra() {
+    setList([{id: uuid(), task: task}])
+    
+  }
 
 
   return(
     <>
-      <input placeholder="O que eu tenho que fazer..." type="text" />
-      <button>Adicionar</button>
+      <input onChange={palavraAdicionnada} placeholder="O que eu tenho que fazer..." type="text" />
+      <button onClick={adicioneiPalavra} >Adicionar</button>
 
       <ul>
-        
+        {
+          List.map(item => (
+            <li key={item.id}>{item.task}</li>
+          ))
+        }
       </ul>
     </>
   )
